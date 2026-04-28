1af7a0b9acf36d09194f6c643f73d83fb6798ddf
Hema-latha-coder <hemalathas889@gmail.com>
2026-04-28 10:13:43 +0530

    new task added

--- Basics/Task1.py
+++ Basics/Task1.py
@@ -5,15 +5,8 @@ print("Baby name: Raphael")
 print('Happy family')
 print("Have a good day")
 
-print("=======Task 2=======")
 
-a,b = 100, 200
 
-print(a) # 100
-print(b) # 200
 
-print("a =", b)
-print('b =', a)
 
-print(a) # 200
-print(b) # 100
+
---
+++ Basics/task2.py
@@ -0,0 +1,10 @@
+a,b = 100, 200
+
+print(a) # 100
+print(b) # 200
+
+print("a =", b)
+print('b =', a)
+
+print(a) # 200
+print(b) # 100
